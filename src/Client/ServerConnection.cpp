dbb92af0f53231e805842c8c27690f8101586e32
Lynix <lynix680@gmail.com>
2018-10-07 21:55:34 +0200

    Fix copyright notices

--- src/Client/ServerConnection.cpp
+++ src/Client/ServerConnection.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2018 Jérôme Leclercq
-// This file is part of the "Erewhon Client" project
+// This file is part of the "Burgwar Client" project
 // For conditions of distribution and use, see copyright notice in LICENSE
 
 #include <Client/ServerConnection.hpp>
--- src/Shared/Protocol/Packets.cpp
+++ src/Shared/Protocol/Packets.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2018 Jérôme Leclercq
-// This file is part of the "Erewhon Shared" project
+// This file is part of the "Burgwar Shared" project
 // For conditions of distribution and use, see copyright notice in LICENSE
 
 #include <Shared/Protocol/Packets.hpp>
