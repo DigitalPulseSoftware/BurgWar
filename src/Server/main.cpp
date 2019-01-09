427c0087d4fd523e3a50ed1c5f0244798d9c6f5f
Lynix <lynix680@gmail.com>
2019-01-09 20:05:30 +0100

    Update main.cpp

--- src/Server/main.cpp
+++ src/Server/main.cpp
@@ -2,6 +2,7 @@
 // This file is part of the "Burgwar Shared" project
 // For conditions of distribution and use, see copyright notice in LICENSE
 
+#include <Nazara/Core/Initializer.hpp>
 #include <Nazara/Network/Network.hpp>
 #include <Server/ServerApp.hpp>
 #include <filesystem>
