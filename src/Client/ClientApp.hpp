fe21681b65d362dec3b4873078055be1034eb2a3
Lynix <lynix680@gmail.com>
2019-02-13 01:13:07 +0100

    Big cleanup

--- src/Client/ClientApp.hpp
+++ src/Client/ClientApp.hpp
@@ -10,13 +10,8 @@
 #include <CoreLib/BurgApp.hpp>
 #include <ClientLib/LocalCommandStore.hpp>
 #include <ClientLib/NetworkReactorManager.hpp>
-#include <Nazara/Graphics/Sprite.hpp>
-#include <Nazara/Math/Angle.hpp>
-#include <Nazara/Network/IpAddress.hpp>
 #include <Nazara/Renderer/RenderWindow.hpp>
 #include <NDK/Application.hpp>
-#include <NDK/Entity.hpp>
-#include <NDK/World.hpp>
 #include <vector>
 
 namespace bw
@@ -35,29 +30,12 @@ namespace bw
 			int Run();
 
 		private:
-			std::shared_ptr<LocalMatch> CreateLocalMatch(ClientSession& session, const Packets::MatchData& matchData);
-
 			std::vector<std::shared_ptr<LocalMatch>> m_localMatches;
 			std::unique_ptr<Match> m_match;
 			std::unique_ptr<ClientSession> m_clientSession;
 			Nz::RenderWindow& m_mainWindow;
 			LocalCommandStore m_commandStore;
 			NetworkReactorManager m_networkReactors;
-#if 0
-			Ndk::EntityHandle m_camera;
-			Ndk::EntityHandle m_burger;
-			Ndk::EntityHandle m_weapon;
-			Ndk::World& m_world;
-			Nz::DegreeAnglef m_attackOriginAngle;
-			Nz::SpriteRef m_burgerSprite;
-			Nz::SpriteRef m_weaponSprite;
-			bool m_isAttacking;
-			bool m_isTargetingRight;
-			bool m_isFacingRight;
-			bool m_isOnGround;
-			bool m_isMoving;
-			float m_attackTimer;
-#endif
 	};
 }
 
