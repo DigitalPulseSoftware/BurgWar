3ab7652ca5838da23d375868d1d7f5a209f1687a
Lynix <lynix680@gmail.com>
2019-06-23 18:46:29 +0200

    Derp.

--- scripts/weapons/rifle.lua
+++ scripts/weapons/rifle.lua
@@ -7,14 +7,17 @@ WEAPON.WeaponOffset = Vec2(0, -40) -- This should not be here
 WEAPON.Cooldown = 0.1
 
 WEAPON.ShootSound = {
-    "sounds/FIREARM_Assault_Rifle_Model_01_Fire_Single_RR1_stereo.wav",
+--[[    "sounds/FIREARM_Assault_Rifle_Model_01_Fire_Single_RR1_stereo.wav",
     "sounds/FIREARM_Assault_Rifle_Model_01_Fire_Single_RR2_stereo.wav",
-    "sounds/FIREARM_Assault_Rifle_Model_01_Fire_Single_RR3_stereo.wav",
+    "sounds/FIREARM_Assault_Rifle_Model_01_Fire_Single_RR3_stereo.wav",]]
+    "placeholder/piou1.wav",
+    "placeholder/piou2.wav",
+    "placeholder/piou3.wav",
 }
 
 function WEAPON:OnAttack()
 	if (CLIENT) then
-		self:PlaySound(self.ShootSound[math.random(1, 3)], true, false, true)
+		self:PlaySound(self.ShootSound[math.random(1, #self.ShootSound)], true, false, true)
 	end
 
 	local shootDir = self:GetDirection()
