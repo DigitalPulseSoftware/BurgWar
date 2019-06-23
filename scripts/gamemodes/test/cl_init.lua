da79b058858dd7a0826156df9912431bdb087d35
Lynix <lynix680@gmail.com>
2019-06-23 18:46:19 +0200

    Fix player camera on death

--- scripts/gamemodes/test/cl_init.lua
+++ scripts/gamemodes/test/cl_init.lua
@@ -1,6 +1,9 @@
 function GM:OnFrame()
-	local cameraCenter = Vec2(-640, -320) + engine_GetPlayerPosition(0)
-	engine_SetCameraPosition(Vec2(math.clamp(cameraCenter.x, 0, 30000), math.clamp(cameraCenter.y, -60000, 3000)))
+	local playerPosition = engine_GetPlayerPosition(0)
+	if (playerPosition) then
+		local cameraCenter = Vec2(-640, -320) + playerPosition
+		engine_SetCameraPosition(Vec2(math.clamp(cameraCenter.x, 0, 30000), math.clamp(cameraCenter.y, -60000, 3000)))
+	end
 end
 
 function GM:OnInit()
