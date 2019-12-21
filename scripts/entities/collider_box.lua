95ec5a5a49adbc15c6e1dd2a8fcc9cf9b5cc72d9
Lynix <lynix680@gmail.com>
2019-12-21 15:37:14 +0100

    Add entity collider_box

---
+++ scripts/entities/collider_box.lua
@@ -0,0 +1,22 @@
+ENTITY.IsNetworked = true
+
+ENTITY.Properties = {
+	{ Name = "friction", Type = PropertyType.Float, Default = 1 },
+	{ Name = "mass", Type = PropertyType.Float, Default = 0 },
+	{ Name = "size", Type = PropertyType.FloatSize },
+}
+
+function ENTITY:Initialize()
+	local size = self:GetProperty("size")
+	local colliderSize = size / 2
+	self:SetCollider(Rect(-colliderSize, colliderSize))
+	self:InitRigidBody(self:GetProperty("mass"), self:GetProperty("friction"))
+
+	if (EDITOR) then
+		self:AddSprite({
+			Color = { r = 200, g = 0, b = 0, a = 180 },
+			RenderOrder = 2000,
+			Size = self:GetProperty("size"),
+		})
+	end
+end
