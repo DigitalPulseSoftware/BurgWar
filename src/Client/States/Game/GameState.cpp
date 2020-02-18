e818bfef8c0d35eb71d10399b9d61290de25ae2d
Lynix <lynix680@gmail.com>
2020-02-18 21:07:22 +0100

    Forgot this file

--- src/Client/States/Game/GameState.cpp
+++ src/Client/States/Game/GameState.cpp
@@ -5,17 +5,16 @@
 #include <Client/States/Game/GameState.hpp>
 #include <Client/ClientApp.hpp>
 #include <ClientLib/LocalMatch.hpp>
-#include <random>
 
 namespace bw
 {
-	GameState::GameState(std::shared_ptr<StateData> stateDataPtr, std::shared_ptr<ClientSession> clientSession, const Packets::MatchData& matchData, std::shared_ptr<VirtualDirectory> assetDirectory, std::shared_ptr<VirtualDirectory> scriptDirectory) :
+	GameState::GameState(std::shared_ptr<StateData> stateDataPtr, std::shared_ptr<ClientSession> clientSession, const Packets::AuthSuccess& authSuccess, const Packets::MatchData& matchData, std::shared_ptr<VirtualDirectory> assetDirectory, std::shared_ptr<VirtualDirectory> scriptDirectory) :
 	AbstractState(std::move(stateDataPtr)),
 	m_clientSession(std::move(clientSession))
 	{
 		StateData& stateData = GetStateData();
 
-		m_match = std::make_shared<LocalMatch>(*stateData.app, stateData.window, &stateData.canvas.value(), *m_clientSession, matchData);
+		m_match = std::make_shared<LocalMatch>(*stateData.app, stateData.window, &stateData.canvas.value(), *m_clientSession, authSuccess, matchData);
 		m_match->LoadAssets(std::move(assetDirectory));
 		m_match->LoadScripts(std::move(scriptDirectory));
 
