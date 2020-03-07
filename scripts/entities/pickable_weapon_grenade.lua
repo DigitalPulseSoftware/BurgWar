4d82ff0c6a151a8d4c923d2f6227e21ab4607683
Lynix <lynix680@gmail.com>
2020-03-07 11:26:02 +0100

    Fix grenade sprite

--- scripts/entities/pickable_weapon_grenade.lua
+++ scripts/entities/pickable_weapon_grenade.lua
@@ -1,5 +1,5 @@
 RegisterClientScript()
-RegisterClientAssets("placeholder/potato.png")
+RegisterClientAssets("grenade.png")
 
 ENTITY.IsNetworked = true
 ENTITY.PlayerControlled = false
@@ -13,8 +13,8 @@ function ENTITY:Initialize()
 
 	if (CLIENT) then
 		self:AddSprite({
-			Scale = Vec2(0.1, 0.1),
-			TexturePath = "placeholder/potato.png"
+			Scale = Vec2(0.25, 0.25),
+			TexturePath = "grenade.png"
 		})
 	end
 end
