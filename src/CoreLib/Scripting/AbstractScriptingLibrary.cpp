c1a8d007656d1616440bf2656f735573b0fb0bcc
Jérôme Leclercq <lynix680@gmail.com>
2020-11-22 13:52:38 +0100

    Script: Add function pprint (pretty print) and extend tostring

--- src/CoreLib/Scripting/AbstractScriptingLibrary.cpp
+++ src/CoreLib/Scripting/AbstractScriptingLibrary.cpp
@@ -12,6 +12,157 @@
 
 namespace bw
 {
+	namespace
+	{
+		int PushAsString(lua_State* L, int idx, bool extended);
+		int PushTableAsString(lua_State* L);
+
+		int PushAsString(lua_State* L, int idx, bool extended)
+		{
+			if (!luaL_callmeta(L, idx, "__tostring"))
+			{
+				int t = lua_type(L, idx);
+
+				switch (t)
+				{
+					case LUA_TNIL:
+						lua_pushliteral(L, "nil");
+						break;
+
+					case LUA_TSTRING:
+					case LUA_TNUMBER:
+						lua_pushvalue(L, idx);
+						break;
+
+					case LUA_TBOOLEAN:
+						if (lua_toboolean(L, idx))
+							lua_pushliteral(L, "true");
+						else
+							lua_pushliteral(L, "false");
+						break;
+
+					case LUA_TTABLE:
+					{
+						if (extended)
+						{
+							lua_pushvalue(L, idx);
+							lua_pushliteral(L, "");
+							lua_createtable(L, 0, 0);
+							PushTableAsString(L);
+							break;
+						}
+					}
+
+					default:
+					{
+						int tt = luaL_getmetafield(L, idx, "__name");
+						const char* name = (tt == LUA_TSTRING) ? lua_tostring(L, idx) : lua_typename(L, t);
+						lua_pushfstring(L, "%s: %p", name, lua_topointer(L, idx));
+						if (tt != LUA_TNIL)
+							lua_replace(L, -2);
+						break;
+					}
+				}
+			}
+			else
+			{
+				if (!lua_isstring(L, -1))
+					luaL_error(L, "'__tostring' must return a string");
+			}
+
+			return 1;
+		}
+
+		int PushTableAsString(lua_State* L)
+		{
+			int tableIndex = lua_absindex(L, -3);
+			int indentIndex = lua_absindex(L, -2);
+			int visitedIndex = lua_absindex(L, -1);
+
+			luaL_checktype(L, tableIndex, LUA_TTABLE); //< table
+			luaL_checktype(L, indentIndex, LUA_TSTRING); //< indentation
+			luaL_checktype(L, visitedIndex, LUA_TTABLE); //< visited
+
+			// Add indentation
+			lua_pushvalue(L, indentIndex);
+			lua_pushstring(L, "  ");
+			lua_concat(L, 2);
+			int newIndentIndex = lua_absindex(L, -1); //< new value on top of the stack is the new indentation
+
+			luaL_Buffer b;
+			luaL_buffinit(L, &b);
+
+			lua_pushvalue(L, indentIndex);
+			luaL_addvalue(&b);
+
+			lua_pushstring(L, "{\n");
+			luaL_addvalue(&b);
+
+			lua_pushnil(L);
+			while (lua_next(L, tableIndex))
+			{
+				bool recursive = false;
+				if (lua_istable(L, -1))
+				{
+					lua_pushvalue(L, -1);
+					lua_gettable(L, visitedIndex);
+					if (lua_isnil(L, -1))
+					{
+						lua_pop(L, 1);
+
+						lua_pushvalue(L, -1);
+						lua_pushboolean(L, true);
+						lua_settable(L, visitedIndex);
+
+						recursive = true;
+					}
+				}
+
+				// Indent
+				lua_pushvalue(L, newIndentIndex);
+				luaL_addvalue(&b);
+
+				// Key
+				PushAsString(L, -2, false);
+				luaL_addvalue(&b);
+
+				// = 
+				lua_pushstring(L, (recursive) ? " = \n" : " = ");
+				luaL_addvalue(&b);
+
+				// Value
+				if (recursive)
+				{
+					lua_pushvalue(L, -1);
+					lua_pushvalue(L, newIndentIndex);
+					lua_pushvalue(L, visitedIndex);
+					PushTableAsString(L);
+				}
+				else
+					PushAsString(L, -1, false);
+
+				luaL_addvalue(&b);
+
+				lua_pushstring(L, "\n");
+				luaL_addvalue(&b);
+
+				lua_pop(L, 1);
+			}
+
+			lua_pop(L, 2); // pop new indentation and visited table
+
+			luaL_addvalue(&b); //< add (and pop) new indentation to the buffer
+
+			lua_pushstring(L, "}");
+			luaL_addvalue(&b);
+
+			lua_pop(L, 1); //< pop table
+
+			luaL_pushresult(&b);
+			return 1;
+		}
+	}
+
 	AbstractScriptingLibrary::~AbstractScriptingLibrary() = default;
 
 	void AbstractScriptingLibrary::RegisterGlobalLibrary(ScriptingContext& context)
@@ -23,17 +174,27 @@ namespace bw
 
 			if (!context.Load(scriptPath.generic_u8string()))
 				throw std::runtime_error("TODO");
+		});
+
+		luaState["tostring"] = [](lua_State* L) -> int
+		{
+			luaL_checkany(L, 1);
+			bool extended = luaL_opt(L, lua_toboolean, 2, false);
+			PushAsString(L, 1, extended);
+			return 1;
 		};
 
-		luaState["print"] = [this](sol::this_state L, sol::variadic_args args)
+		auto PrintExt = [&context](sol::this_state L, sol::variadic_args args, bool extended)
 		{
 			bool first = true;
 
 			std::ostringstream oss;
 			for (auto v : args)
 			{
+				PushAsString(L, v.stack_index(), extended);
+
 				std::size_t length;
-				const char* str = luaL_tolstring(L, v.stack_index(), &length);
+				const char* str = lua_tolstring(L, -1, &length);
 				oss << std::string(str, length);
 				if (!first)
 					oss << "\t";
@@ -41,8 +202,18 @@ namespace bw
 				first = false;
 			}
 
-		};
 			context.Print(oss.str());
+		};
+
+		luaState["pprint"] = ExceptToLuaErr([=](sol::this_state L, sol::variadic_args args)
+		{
+			PrintExt(L, args, true);
+		});
+
+		luaState["print"] = ExceptToLuaErr([=](sol::this_state L, sol::variadic_args args)
+		{
+			PrintExt(L, args, false);
+		});
 	}
 
 	void AbstractScriptingLibrary::RegisterMetatableLibrary(ScriptingContext& context)
