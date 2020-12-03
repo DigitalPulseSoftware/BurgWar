15a5f03d466f6faf102f2dde99401ed779337fa6
Jérôme Leclercq <lynix680@gmail.com>
2020-12-03 20:09:54 +0100

    Script: propagate coroutines errors
    coroutine.resume acts like pcall and silence some errors

--- scripts/autorun/libs/animations.lua
+++ scripts/autorun/libs/animations.lua
@@ -5,7 +5,7 @@ function animation.PositionByOffset(entity, from, to, time)
 	local co, main = coroutine.running()
 	assert(not main, "Must be called from a coroutine")
 
-	engine_AnimatePositionByOffsetSq(entity, from, to, time, function () coroutine.resume(co) end)
+	engine_AnimatePositionByOffsetSq(entity, from, to, time, function () assert(coroutine.resume(co)) end)
 	coroutine.yield()
 end
 
@@ -13,6 +13,6 @@ function animation.Rotate(entity, from, to, time)
 	local co, main = coroutine.running()
 	assert(not main, "Must be called from a coroutine")
 
-	engine_AnimateRotation(entity, from, to, time, function () coroutine.resume(co) end)
+	engine_AnimateRotation(entity, from, to, time, function () assert(coroutine.resume(co)) end)
 	coroutine.yield()
 end
--- scripts/autorun/libs/timer.lua
+++ scripts/autorun/libs/timer.lua
@@ -4,6 +4,6 @@ function timer.Sleep(ms)
 	local co, main = coroutine.running()
 	assert(not main, "Must be called from a coroutine")
 
-	timer.Create(ms, function () coroutine.resume(co) end)
+	timer.Create(ms, function () assert(coroutine.resume(co)) end)
 	coroutine.yield()
 end
