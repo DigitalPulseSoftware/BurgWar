0d84ed68010f4da2de73df2cc3674d1b52f70929
Jérôme Leclercq <lynix680@gmail.com>
2020-12-07 18:38:48 +0100

    Minor stuff

--- scripts/autorun/libs/timer.lua
+++ scripts/autorun/libs/timer.lua
@@ -2,7 +2,7 @@ RegisterClientScript()
 
 function timer.Sleep(ms)
 	local co, main = coroutine.running()
-	assert(not main, "Must be called from a coroutine")
+	assert(not main, "must be called from a coroutine")
 
 	timer.Create(ms, function () assert(coroutine.resume(co)) end)
 	coroutine.yield()
