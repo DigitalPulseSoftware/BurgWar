4749db9d7037031d164ae6320bf17179d6167a69
de Macedo <demacedius@pop-os.localdomain>
2021-04-15 00:25:07 +0200

    change function

--- scripts/entities/debug_text.lua
+++ scripts/entities/debug_text.lua
@@ -3,8 +3,24 @@ RegisterClientScript()
 local entity = ScriptedEntity({
     IsNetworked = true,
     Properties = {
-        Position = { x, y}
-        text = self.Text AddText(Default = " ")
+        {Name = "text", Type = PropertyType.string , shared = true},
+        {Name = "Lifetime", Type = PropertyType.Float , shared = true}
     }
 })
+
+if (not EDITOR) then
+    entity:On("init", function(self)
+        local lifetime = self:GetProperty("Lifetime")
+        if (lifetime > 0) then
+        self:SetLifeTime(lifetime)
+       end
+    end)
 end
+
+
+if (CLIENT) then
+    entity:On("init", function(self)
+        local text = self:GetProperty("text")
+        local color = {r=85 , g=15 , b= 15, a = 100 }
+        local textWidth = 5
+    )
