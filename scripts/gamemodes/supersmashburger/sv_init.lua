b6fbd2117c235673e1de5041ea59f7bf16042205
Jérôme Leclercq <lynix680@gmail.com>
2021-07-04 18:02:44 +0200

    Gamemodes/SSB: Improve spawnpoint choosing

--- scripts/gamemodes/supersmashburger/sv_init.lua
+++ scripts/gamemodes/supersmashburger/sv_init.lua
@@ -17,3 +17,28 @@ gamemode:On("MapInit", function (self)
 		match.CreateEntity(creationInfo)
 	end
 end)
+
+function gamemode:ChoosePlayerSpawnPosition()
+	local spawnpoints = {}
+
+	for _, spawnpoint in pairs(match.GetEntitiesByClass("entity_spawnpoint")) do
+		local spawnPos = spawnpoint:GetPosition()
+		local spawnLayer = spawnpoint:GetLayerIndex()
+
+		local minDistance = math.huge
+		for _, playerEntity in pairs(match.GetEntitiesByClass("entity_ssb_burger", spawnLayer)) do
+			minDistance = math.min(minDistance, playerEntity:GetPosition():SquaredDistance(spawnPos))
+		end
+
+		table.insert(spawnpoints, {
+			position = spawnPos,
+			layer = spawnLayer,
+			score = minDistance
+		})
+	end
+
+	table.sort(spawnpoints, function (a, b) return a.score > b.score end)
+
+	local choosedSpawnpoint = spawnpoints[1]
+	return choosedSpawnpoint.position, choosedSpawnpoint.layer
+end
