c19f836dc5c0ab8eeb35f9867127d79b56f5f1c5
Jérôme Leclercq <lynix680@gmail.com>
2021-08-16 20:28:23 +0200

    App/MapTool: Rework to be able to handle multiples maps (CI will now compile all maps)

--- src/MapTool/main.cpp
+++ src/MapTool/main.cpp
@@ -5,6 +5,7 @@
 #include <CoreLib/Map.hpp>
 #include <Main/Main.hpp>
 #include <cxxopts.hpp>
+#include <fmt/format.h>
 #include <filesystem>
 #include <stdexcept>
 
@@ -12,60 +13,112 @@ int BurgWarMapTool(int argc, char* argv[])
 {
 	cxxopts::Options options("BurgWarMapTool", "Tool for compiling BurgWar maps in CLI");
 	options.add_options()
-		("c,compile", "Compilation output", cxxopts::value<std::string>())
-		("i,input", "Input file", cxxopts::value<std::string>())
+		("c,compile", "Compile input maps to binary map format")
+		("i,input", "Input file(s)", cxxopts::value<std::vector<std::string>>())
+		("o,output", "Output folder", cxxopts::value<std::string>()->default_value("."), "path")
+		("s,show", "Show informations about the map (default)")
 		("h,help", "Print usage")
 	;
 
 	options.parse_positional("input");
+	options.positional_help("MAPS");
 
 	try
 	{
 		auto result = options.parse(argc, argv);
 		if (result.count("help") > 0)
 		{
-			std::cout << options.help() << std::endl;
+			fmt::print("{}\n", options.help());
 			return EXIT_SUCCESS;
 		}
 
 		if (result.count("input") == 0)
 		{
-			std::cout << "No input file\n" << options.help() << std::endl;
+			fmt::print("no input files\n{}\n", options.help());
 			return EXIT_SUCCESS;
 		}
 
-		std::string input = result["input"].as<std::string>();
-
-		bw::Map map;
-		if (std::filesystem::is_directory(input))
-			map = bw::Map::LoadFromDirectory(input);
-		else if (std::filesystem::is_regular_file(input))
-			map = bw::Map::LoadFromBinary(input);
-		else if (std::filesystem::exists(input))
-			throw std::runtime_error("input map is neither a directory nor a binary");
-		else
-			throw std::runtime_error("input map doesn't exist");
-
-		if (result.count("compile") > 0)
+		bool first = true;
+		std::vector<std::string> inputMaps = result["input"].as<std::vector<std::string>>();
+		for (const std::string& inputMap : result["input"].as<std::vector<std::string>>())
 		{
-			std::filesystem::path outputPath = result["compile"].as<std::string>();
-			if (!outputPath.has_extension())
-				outputPath.replace_extension("bmap");
+			std::filesystem::path inputPath = inputMap;
+			std::filesystem::path mapName;
 
-			if (!map.Compile(outputPath))
-				throw std::runtime_error("failed to compile map: failed to open " + outputPath.generic_u8string());
+			if (!first)
+				fmt::print("-----\n");
 
-			std::cout << "Successfully compiled map to " << outputPath.generic_u8string() << std::endl;
+			first = false;
+
+			bw::Map map;
+
+			try
+			{
+				if (std::filesystem::is_directory(inputPath))
+				{
+					map = bw::Map::LoadFromDirectory(inputPath);
+					if (inputPath.has_filename())
+						mapName = inputPath.filename(); //< foo/bar => bar
+					else
+						mapName = inputPath.parent_path().filename(); //< foo/bar/ => bar
+				}
+				else if (std::filesystem::is_regular_file(inputPath))
+				{
+					map = bw::Map::LoadFromBinary(inputPath);
+					mapName = inputPath.stem(); //< foo/bar.bmap => bar
+				}
+				else if (std::filesystem::exists(inputPath))
+					throw std::runtime_error(inputMap + " is neither a directory nor a binary");
+				else
+					throw std::runtime_error(inputMap + " doesn't exist");
+			}
+			catch (const std::exception& e)
+			{
+				fmt::print(stderr, "{0}: {1}\n", inputMap, e.what());
+				continue;
+			}
+
+			if (result.count("compile") > 0)
+			{
+				std::filesystem::path outputPath = result["output"].as<std::string>();
+				if (!std::filesystem::is_directory(outputPath))
+					std::filesystem::create_directories(outputPath);
+
+				outputPath /= mapName;
+				if (outputPath.extension() != "bmap")
+					outputPath += ".bmap";
+
+				if (!map.Compile(outputPath))
+					throw std::runtime_error("failed to compile map: failed to open " + outputPath.generic_u8string());
+
+				fmt::print("successfully compiled map {0} to {1}\n", inputMap, outputPath.generic_u8string());
+			}
+			else
+			{
+				// Show info about the map
+				const auto& mapInfo = map.GetMapInfo();
+
+				fmt::print("{input_map} info:\n- Name: {name}\n- Description: {desc}\n- Author: {author}\n", fmt::arg("input_map", inputMap), fmt::arg("name", mapInfo.name), fmt::arg("desc", mapInfo.description), fmt::arg("author", mapInfo.author));
+
+				std::size_t layerCount = map.GetLayerCount();
+				fmt::print("\nThis map has {} layer(s):\n", layerCount);
+				for (std::size_t i = 0; i < layerCount; ++i)
+				{
+					const auto& layer = map.GetLayer(i);
+					fmt::print("- layer #{} ({}) has {} entities\n", i, layer.name, layer.entities.size());
+				}
+			}
+
+			first = false;
 		}
 	}
 	catch (const cxxopts::OptionException& e)
 	{
-		std::cout << e.what() << "\n";
-		std::cout << options.help() << std::endl;
+		fmt::print(stderr, "{}\n{}\n", e.what(), options.help());
 	}
 	catch (const std::exception& e)
 	{
-		std::cout << e.what() << std::endl;
+		fmt::print(stderr, "{}\n", e.what());
 	}
 
 	return EXIT_SUCCESS;
