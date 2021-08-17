a13413981ea36aa26f3d5dec474d740d984a7080
Elanis <elanis@hotmail.com>
2021-08-17 19:46:00 +0200

    Burgwar Docker image - First working version
    TODO:
    - stop using git clone and use local repo
    - use environment variables instead of serverconfig.lua
    - create empty assets folder
    - use /home/burgwar instead of /home/burgwar/BurgWar
    - use custom map file
    - remove nazara legacy apt when we migrated

---
+++ docker/serverconfig.lua
@@ -0,0 +1,21 @@
+Debug = {
+	SendServerState = true
+}
+Resources = {
+	AssetDirectory = "../../assets",
+	ModDirectory = "../../mods",
+	ScriptDirectory  = "../../scripts"
+}
+ServerSettings = {
+	FastDownloadURLs = [[
+https://burgwar.digitalpulse.software/resources
+	]],
+	MasterServers = [[
+https://bwmasterserver.digitalpulse.software
+	]],
+	Gamemode = "deathmatch",
+	MapPath = "beta_map.bmap",
+	Name = "no name set",
+	Description = "a description of your server"
+	TickRate = 33,
+}
