98cd569beee072f9052557d46fc5ee0adcc74305
Jérôme Leclercq <lynix680@gmail.com>
2021-08-30 11:15:41 +0200

    Core/ConfigFile: Rework loading to be much more verbose about errors
    It can now prints ignored configs and will log all errors/warnings instead of stopping at the first

--- src/CoreLib/ConfigFile.cpp
+++ src/CoreLib/ConfigFile.cpp
@@ -44,129 +44,46 @@ namespace bw
 
 		try
 		{
-			if (auto result = lua.safe_script_file(path); !result.valid())
+			sol::environment configEnv(lua, sol::create, lua.globals());
+
+			if (auto result = lua.safe_script_file(path, configEnv, sol::load_mode::text); !result.valid())
 			{
 				sol::error err = result;
 				bwLog(m_app.GetLogger(), LogLevel::Error, "failed to load config {0}: {1}", path, err.what());
 				return false;
 			}
 
-			lua_State* L = lua.lua_state();
+			bool hasError = false;
 
-			auto PushLuaVariable = [L](const std::string& optionName)
+			Nz::Bitset<> missingOptions(m_options.size(), true);
+			if (!LoadSection(configEnv, m_globalSection, missingOptions))
+				hasError = true;
+
+			for (std::size_t i = missingOptions.FindFirst(); i != missingOptions.npos; i = missingOptions.FindNext(i))
 			{
-				lua_getglobal(L, "_G");
+				ConfigOption& option = m_options[i];
 
-				return ForEachSection(optionName, [L](std::string_view variable)
-				{
-					lua_getfield(L, -1, std::string(variable).data());
-					lua_remove(L, -2);
-
-					return lua_isnoneornil(L, -1) == 0;
-				});
-			};
-
-			// TODO use sections
-			for (ConfigOption& option : m_options)
-			{
 				bool hasDefaultDefault = std::visit([](auto&& arg)
 				{
 					return arg.defaultValue.has_value();
 				}, option.data);
 
-				if (!PushLuaVariable(option.name))
+				if (!hasDefaultDefault)
 				{
-					if (hasDefaultDefault)
-						continue;
-
 					bwLog(m_app.GetLogger(), LogLevel::Error, "missing config option \"{0}\"", option.name);
-					return false;
+					hasError = true;
 				}
-
-				try
+				else
 				{
-					std::visit([&](auto&& option)
+					std::visit([&](auto&& arg)
 					{
-						using T = std::decay_t<decltype(option)>;
-						using ArgType = std::decay_t<decltype(option.value)>;
-
-						ArgType value;
-
-						// Get value
-						if constexpr (std::is_same_v<T, BoolOption>)
-						{
-							if (!lua_isboolean(L, -1))
-								throw std::runtime_error("expected boolean, got " + std::string(lua_typename(L, -1)));
-
-							value = lua_toboolean(L, -1);
-						}
-						else if constexpr (std::is_same_v<T, FloatOption>)
-						{
-							if (!lua_isnumber(L, -1))
-								throw std::runtime_error("expected number, got " + std::string(lua_typename(L, -1)));
-
-							value = luaL_checknumber(L, -1);
-						}
-						else if constexpr (std::is_same_v<T, IntegerOption>)
-						{
-							if (!lua_isnumber(L, -1))
-								throw std::runtime_error("expected number, got " + std::string(lua_typename(L, -1)));
-
-							value = luaL_checkinteger(L, -1);
-						}
-						else if constexpr (std::is_same_v<T, StringOption>)
-						{
-							if (!lua_isstring(L, -1))
-								throw std::runtime_error("expected string, got " + std::string(lua_typename(L, -1)));
-
-							value = luaL_checkstring(L, -1);
-						}
-						else
-							static_assert(AlwaysFalse<T>::value, "non-exhaustive visitor");
-
-						// Check bounds
-						if constexpr (std::is_same_v<T, FloatOption> || std::is_same_v<T, IntegerOption>)
-						{
-							if (value < option.minBounds)
-								throw std::runtime_error("option value is under bounds (" + std::to_string(value) + " < " + std::to_string(option.minBounds) + ')');
-							else if (value > option.maxBounds)
-								throw std::runtime_error("option value is over bounds (" + std::to_string(value) + " > " + std::to_string(option.maxBounds) + ')');
-						}
-
-						if constexpr (!std::is_same_v<T, BoolOption>)
-						{
-							if (option.validation)
-							{
-								auto valueOrErr = option.validation(std::move(value));
-								if (!valueOrErr)
-									throw std::runtime_error("option value failed validation: " + valueOrErr.error());
-
-								value = std::move(valueOrErr).value();
-							}
-						}
-
-						option.value = std::move(value);
-
+						bwLog(m_app.GetLogger(), LogLevel::Debug, "option \"{0}\" has no value, setting it to default value ({1})...", option.name, arg.defaultValue.value());
 					}, option.data);
 				}
-				catch (const std::exception& e)
-				{
-					bwLog(m_app.GetLogger(), LogLevel::Error, "failed to load \"{0}\": {1}", option.name, e.what());
-
-					if (!hasDefaultDefault)
-						return false;
-				}
-				catch (...)
-				{
-					bwLog(m_app.GetLogger(), LogLevel::Error, "failed to load \"{0}\": {1}", option.name, lua_tostring(L, -1));
-					lua_pop(L, lua_gettop(L));
-
-					if (!hasDefaultDefault)
-						return false;
-				}
-
-				lua_pop(L, 1);
 			}
+
+			if (hasError)
+				return false;
 		}
 		catch (const sol::error& e)
 		{
@@ -186,19 +103,126 @@ namespace bw
 			return false;
 		}
 
-		for (auto&& [sectionName, section] : m_subsections)
-		{
-			if (!sectionName.empty())
-			{
-				file << sectionName << " = {\n";
-				SaveSectionToFile(file, *section, 1);
-				file << "}\n";
-			}
-			else
-				SaveSectionToFile(file, *section, 0);
-		}
+		SaveSectionToFile(file, m_globalSection, 0);
 
 		return file.good();
+	}
+
+	bool ConfigFile::LoadSection(const sol::table& table, ConfigSection& section, Nz::Bitset<>& missingOptions, const std::string& prefix)
+	{
+		bool hasError = false;
+
+		table.for_each([&](const sol::object& key, const sol::object& value)
+		{
+			std::string keyName = key.as<std::string>();
+
+			if (auto sectionIt = section.subsections.find(keyName); sectionIt != section.subsections.end())
+			{
+				if (!value.is<sol::table>())
+				{
+					bwLog(m_app.GetLogger(), LogLevel::Error, "\"{0}{1}\" is a subsection and was expected to be a table (got {2})", prefix, keyName, sol::type_name(table.lua_state(), value.get_type()));
+					return;
+				}
+
+				if (!LoadSection(value.as<sol::table>(), *sectionIt->second, missingOptions, prefix + keyName + "."))
+					hasError = true;
+			}
+			else if (auto optionIt = section.options.find(keyName); optionIt != section.options.end())
+			{
+				ConfigOption& option = m_options[optionIt->second];
+
+				bool hasDefaultDefault = std::visit([](auto&& arg)
+				{
+					return arg.defaultValue.has_value();
+				}, option.data);
+
+				try
+				{
+					std::visit([&](auto&& option)
+					{
+						using T = std::decay_t<decltype(option)>;
+						using ArgType = std::decay_t<decltype(option.value)>;
+
+						ArgType optionValue;
+
+						// Get value
+						if constexpr (std::is_same_v<T, BoolOption>)
+						{
+							if (!value.is<bool>())
+								throw std::runtime_error("expected boolean, got " + sol::type_name(table.lua_state(), value.get_type()));
+
+							optionValue = value.as<bool>();
+						}
+						else if constexpr (std::is_same_v<T, FloatOption>)
+						{
+							if (!value.is<double>())
+								throw std::runtime_error("expected number, got " + sol::type_name(table.lua_state(), value.get_type()));
+
+							optionValue = value.as<double>();
+						}
+						else if constexpr (std::is_same_v<T, IntegerOption>)
+						{
+							if (!value.is<long long>())
+								throw std::runtime_error("expected number, got " + sol::type_name(table.lua_state(), value.get_type()));
+
+							optionValue = value.as<long long>();
+						}
+						else if constexpr (std::is_same_v<T, StringOption>)
+						{
+							if (!value.is<std::string>())
+								throw std::runtime_error("expected string, got " + sol::type_name(table.lua_state(), value.get_type()));
+
+							optionValue = value.as<std::string>();
+						}
+						else
+							static_assert(AlwaysFalse<T>::value, "non-exhaustive visitor");
+
+						// Check bounds
+						if constexpr (std::is_same_v<T, FloatOption> || std::is_same_v<T, IntegerOption>)
+						{
+							if (optionValue < option.minBounds)
+								throw std::runtime_error("option value is under bounds (" + std::to_string(optionValue) + " < " + std::to_string(option.minBounds) + ')');
+							else if (optionValue > option.maxBounds)
+								throw std::runtime_error("option value is over bounds (" + std::to_string(optionValue) + " > " + std::to_string(option.maxBounds) + ')');
+						}
+
+						if constexpr (!std::is_same_v<T, BoolOption>)
+						{
+							if (option.validation)
+							{
+								auto valueOrErr = option.validation(std::move(optionValue));
+								if (!valueOrErr)
+									throw std::runtime_error("option value failed validation: " + valueOrErr.error());
+
+								optionValue = std::move(valueOrErr).value();
+							}
+						}
+
+						option.value = std::move(optionValue);
+
+					}, option.data);
+
+					missingOptions.Reset(option.index);
+				}
+				catch (const std::exception& e)
+				{
+					bwLog(m_app.GetLogger(), LogLevel::Error, "failed to load \"{0}{1}\": {2}", prefix, option.name, e.what());
+
+					if (!hasDefaultDefault)
+					{
+						hasError = true;
+						return;
+					}
+				}
+			}
+			else
+			{
+				bwLog(m_app.GetLogger(), LogLevel::Warning, "unknown config section \"{0}{1}\"", prefix, keyName);
+				return;
+			}
+		});
+
+		return hasError;
 	}
 
 	void ConfigFile::RegisterConfig(std::string optionName, ConfigData data)
@@ -209,6 +233,7 @@ namespace bw
 		auto& option = m_options.emplace_back();
 		option.name = optionName;
 		option.data = std::move(data);
+		option.index = optionIndex;
 
 		m_optionByName.emplace(std::move(optionName), optionIndex);
 
@@ -218,7 +243,7 @@ namespace bw
 		{
 			std::string name(sectionName);
 
-			auto& subsections = (section) ? section->subsections : m_subsections;
+			auto& subsections = (section) ? section->subsections : m_globalSection.subsections;
 
 			auto it = subsections.find(name);
 			if (it == subsections.end())
@@ -235,10 +260,7 @@ namespace bw
 		[&](std::string_view varName)
 		{
 			if (!section)
-			{
-				auto it = m_subsections.emplace(std::string(), std::make_unique<ConfigSection>()).first;
-				section = it->second.get();
-			}
+				section = &m_globalSection;
 
 			section->options.emplace(std::string(varName), optionIndex);
 			return true;
@@ -284,11 +306,16 @@ namespace bw
 				file << "\n";
 		}
 
+		bool isGlobalSection = section.sectionName.empty();
 		for (auto&& [sectionName, sectionData] : section.subsections)
 		{
 			file << indent << sectionName << " = {\n";
 			SaveSectionToFile(file, *sectionData, indentCount + 1);
-			file << indent << "},\n";
+			file << indent << "}";
+			if (!isGlobalSection)
+				file << ",";
+
+			file << "\n";
 		}
 	}
 
